37762b103169df604565abbc3ed9cb2432b7596d
jasonweiyi <jasonweiyi@gmail.com>
2020-02-01 21:27:57 +0800

    Update and rename train_track_door.js to train_track_gate.js

--- source/train_track_gate.js
+++ source/train_track_gate.js
@@ -1,14 +1,28 @@
+// This program constrols the gate on a wooden train track.
+// It requies the OLED and sonar extensions from the Makecode IDE.
+
+// The distance variable stores the distance from the sonar.
 let distance = 0
+
+// Initialize LED screen.
 OLED.init(128, 64)
+
+// Move servo to 0 position.
 pins.servoWritePin(AnalogPin.P7, 0)
+
 basic.forever(function () {
+    // Read a distance from sonar.
     distance = sonar.ping(
     DigitalPin.P4,
     DigitalPin.P9,
     PingUnit.Centimeters
     )
+    
+    // Displace the read distance.
     OLED.clear()
     OLED.writeNumNewLine(distance)
+    
+    // Open the gate if distance is small enough.
     if (distance < 3) {
         pins.servoWritePin(AnalogPin.P7, 90)
         control.waitMicros(8000000)
